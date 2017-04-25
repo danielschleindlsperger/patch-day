4df7ab4bc9d170936d3773560cfec84146a54a01
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2017-04-25 16:49:34 +0200

    delete companies from overview page

--- resources/assets/js/pages/company/Companies.vue
+++ resources/assets/js/pages/company/Companies.vue
@@ -1,41 +1,54 @@
 <template>
-    <v-card>
-        <v-card-text>
-            <div class="button-row">
-                <v-btn primary dark>
-                    <v-icon class="white--text text--darken-2">add_circle
-                    </v-icon>
-                </v-btn>
-            </div>
-            <v-list>
-                <v-list-item v-for="item in list" :key="item.id">
-                    <v-list-tile avatar router :href="item.href">
-                        <v-list-tile-avatar>
-                            <v-icon>business</v-icon>
-                        </v-list-tile-avatar>
-                        <v-list-tile-content>
-                            <v-list-tile-title>{{ item.name }}
-                            </v-list-tile-title>
-                        </v-list-tile-content>
-                        <v-list-tile-action>
-                            <v-btn icon ripple>
-                                <v-icon class="grey--text text--lighten-1">
-                                    info
-                                </v-icon>
-                            </v-btn>
-                        </v-list-tile-action>
-                    </v-list-tile>
-                </v-list-item>
-            </v-list>
-        </v-card-text>
-    </v-card>
+    <div>
+        <v-card>
+            <v-card-text>
+                <div class="button-row">
+                    <v-btn primary dark>
+                        <v-icon class="white--text text--darken-2">add_circle
+                        </v-icon>
+                    </v-btn>
+                </div>
+                <v-list>
+                    <v-list-item v-for="item in list" :key="item.id">
+                        <v-list-tile avatar router :href="item.href">
+                            <v-list-tile-avatar>
+                                <v-icon>business</v-icon>
+                            </v-list-tile-avatar>
+                            <v-list-tile-content>
+                                <v-list-tile-title>{{ item.name }}
+                                </v-list-tile-title>
+                            </v-list-tile-content>
+                            <v-list-tile-action>
+                                <v-btn icon ripple
+                                       @click.native="deleteItem($event, item)">
+                                    <v-icon class="grey--text">
+                                        delete
+                                    </v-icon>
+                                </v-btn>
+                            </v-list-tile-action>
+                        </v-list-tile>
+                    </v-list-item>
+                </v-list>
+            </v-card-text>
+        </v-card>
+        <delete-company></delete-company>
+    </div>
 </template>
 
 <script>
+  import eventBus from 'components/event-bus'
+
+  import DeleteCompany from 'pages/company/DeleteCompany'
+
   export default {
+    components: {
+      DeleteCompany,
+    },
     data() {
       return {
-        list: []
+        list: [],
+        deleteCompany: {},
+        modalOpen: false,
       }
     },
     mounted() {
@@ -48,12 +61,27 @@
         .catch(error => {
           error.response.data
         })
+
+      eventBus.$on('company.deleted', payload => {
+        const COMPANY_ID = payload[0].id
+        console.log(COMPANY_ID)
+        // remove deleted item from list
+        let newList = this.list.filter((item) => {
+          return item.id !== COMPANY_ID
+        })
+        this.list = newList
+      })
     },
     methods: {
       addLinks() {
         this.list.forEach(item => {
           item.href = `/companies/${item.id}`
         })
+      },
+      deleteItem(event, item) {
+        event.preventDefault()
+        event.stopPropagation()
+        eventBus.$emit('company.delete.modal', item);
       }
     }
   }
---
+++ resources/assets/js/pages/company/DeleteCompany.vue
@@ -0,0 +1,56 @@
+<template>
+    <v-dialog v-model="isOpen">
+        <v-card>
+            <v-card-row>
+                <v-card-title>Delete {{ company.name }} ?</v-card-title>
+            </v-card-row>
+            <v-card-row>
+                <v-card-text>
+                    Deleting {{ company.name
+                    }} may have sideeffects and the deletion may not be reversible.
+                </v-card-text>
+            </v-card-row>
+            <v-card-row actions>
+                <v-btn class="green--text darken-1" flat="flat"
+                       @click.native="isOpen = false">PLs no
+                </v-btn>
+                <v-btn class="green--text darken-1" flat="flat"
+                       @click.native="deleteCompany">Delete
+                </v-btn>
+            </v-card-row>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+  import eventBus from 'components/event-bus'
+  export default {
+    name: 'delete-company',
+    data() {
+      return {
+        isOpen: false,
+        company: {}
+      }
+    },
+    mounted () {
+      eventBus.$on('company.delete.modal', company => {
+        this.company = company
+        this.isOpen = true
+      })
+    },
+    methods: {
+      deleteCompany() {
+        this.isOpen = false
+        this.$http.delete(`/companies/${this.company.id}`)
+          .then(response => {
+            if (response.status === 200) {
+              eventBus.$emit('company.deleted', [this.company])
+            }
+          })
+          .catch(error => {
+
+          })
+      }
+    }
+  }
+</script>
