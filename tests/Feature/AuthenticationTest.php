873d70e842f3c880c80e445dcf7090de6c9e6fe3
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2017-04-28 19:23:15 +0200

    authentication test

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -9,9 +10,38 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class AuthenticationTest extends TestCase
 {
+    use DatabaseTransactions;
+    use DatabaseMigrations;
+
+    protected $user;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create([
+            'name' => 'Fake User',
+            'email' => 'fake@example.com',
+            'role' => 'admin',
+        ]);
+    }
+
+    /** @test */
     public function a_user_can_login()
     {
-        // TODO: write test
+        // unauthenticated
+        $request = $this->json('GET', '/users/me');
+        $request->assertStatus(401);
+
+        // authenticated
+        $this->actingAs($this->user);
+        $request = $this->json('GET', '/users/me');
+        $request->assertStatus(200);
+        $request->assertJsonFragment([
+            'name' => $this->user->name,
+            'email' => $this->user->email,
+            'role' => $this->user->role,
+        ]);
     }
 
     public function a_user_can_logout()
