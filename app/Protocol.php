4a2ea9d6e0e40bed1895613c6cf725779d2481cb
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2017-05-08 14:38:20 +0200

    return protocol date as date formatted string

--- app/Protocol.php
+++ app/Protocol.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Protocol extends Model
@@ -15,13 +16,24 @@ class Protocol extends Model
         'comment', 'done', 'due_date', 'protocol_number', 'patch_day_id',
     ];
 
-    protected $dates = ['due_date'];
-
     protected $casts = [
         'done' => 'boolean',
+        'due_date' => 'date',
         'protocol_number' => 'integer',
         'patch_day_id' => 'integer',
     ];
+
+    /**
+     * Get the date in actual date format
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    public function getDueDateAttribute($value)
+    {
+        return Carbon::parse($value)->toDateString();
+    }
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
