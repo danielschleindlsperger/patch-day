c388acc9b94dff7b51cc60a40164b6e936cc549b
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2017-05-09 14:00:45 +0200

    upcoming patch days on dashboard

---
+++ resources/assets/js/components/upcoming-patch-days/Upcoming.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-card>
+        <v-card-row class="primary">
+            <v-card-title>
+                <span class="white--text">Upcoming PatchDays</span>
+            </v-card-title>
+        </v-card-row>
+        <v-card-text>
+            <v-list>
+                <v-list-item v-for="protocol in protocols"
+                             :key="protocol.id">
+                    <v-list-tile router avatar
+                                 :href="'/protocols/' + protocol.id">
+                        <v-list-tile-avatar>
+                            <img src="/img/company_dummy.png"/>
+                        </v-list-tile-avatar>
+                        <v-list-tile-content>
+                            <v-list-tile-title>
+                                {{ protocol.patch_day.project.name }}
+                            </v-list-tile-title>
+                            <v-list-tile-sub-title>
+                                <!-- Display in days from now aswell -->
+                                Due on {{ protocol.due_date | Date }}
+                            </v-list-tile-sub-title>
+                        </v-list-tile-content>
+                        <v-list-tile-action>
+                            <v-icon>navigate_next</v-icon>
+                        </v-list-tile-action>
+                    </v-list-tile>
+                </v-list-item>
+            </v-list>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+  import filters from 'mixins/filters'
+  export default {
+    name: 'upcoming',
+    mixins: [filters],
+    data() {
+      return {
+        protocols: {},
+      }
+    },
+    mounted() {
+      this.$http.get('/protocols/upcoming')
+        .then(response => {
+          this.protocols = response.data
+          console.log(response.data)
+        })
+        .catch(error => {
+          console.error(error)
+          eventBus.$emit('info.snackbar', error.response.data.error)
+          if (error.response.status === 404) {
+            this.$router.push({name: 'not-found'})
+          }
+        })
+    },
+  }
+</script>
--- resources/assets/js/pages/Dashboard.vue
+++ resources/assets/js/pages/Dashboard.vue
@@ -1,9 +1,19 @@
 <template>
-    <div>DASHBOARD PAGE</div>
+    <v-container>
+        <v-row>
+            <v-col xs12 lg6>
+                <upcoming></upcoming>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
+  import Upcoming from 'components/upcoming-patch-days/Upcoming'
   export default {
+    components: {
+      Upcoming,
+    },
     mounted() {
       this.$http.get('/users/me')
         .then(response => {
