a5f4f2251a69641ee7d29515979a388970a77d4f
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2017-06-12 17:19:54 +0200

    'more' menu for toolbar with logout

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -1,24 +1,40 @@
 <template>
     <v-app id="patch-day" class="elevation-1">
         <side-bar :sidebar="sidebar"></side-bar>
-        <v-toolbar class="primary">
-            <v-toolbar-side-icon class="white--text"
+        <v-toolbar class="primary" light>
+            <v-toolbar-side-icon light
                     @click.native.stop="sidebar.open = !sidebar.open"/>
-            <v-toolbar-title class="hidden-sm-and-down white--text">PatchDay
-            </v-toolbar-title>
-            <v-toolbar-items class="hidden-sm-and-down">
-                <v-toolbar-item ripple router href="/">
+            <v-toolbar-title>PatchDay</v-toolbar-title>
+            <v-toolbar-items>
+                <v-toolbar-item class="hidden-sm-and-down"
+                                ripple router href="/">
                     Dashboard
                 </v-toolbar-item>
-                <v-toolbar-item ripple router href="/companies">
+                <v-toolbar-item class="hidden-sm-and-down"
+                                ripple router href="/companies">
                     Companies
                 </v-toolbar-item>
-                <v-toolbar-item ripple router href="/projects">
+                <v-toolbar-item class="hidden-sm-and-down"
+                                ripple router href="/projects">
                     Projects
                 </v-toolbar-item>
-                <v-toolbar-item ripple router href="/users">
+                <v-toolbar-item class="hidden-sm-and-down"
+                                ripple router href="/users">
                     Users
                 </v-toolbar-item>
+                <v-menu left bottom offset-y>
+                    <v-btn icon="icon" slot="activator" light>
+                        <v-icon>more_vert</v-icon>
+                    </v-btn>
+                    <v-list>
+                        <v-list-item>
+                            <v-list-tile>
+                                <v-list-tile-title @click="logout">Logout
+                                </v-list-tile-title>
+                            </v-list-tile>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
             </v-toolbar-items>
         </v-toolbar>
         <main>
@@ -45,7 +61,20 @@
     components: {
       SideBar,
       InfoBar,
-    }
+    },
+    methods: {
+      logout() {
+        this.$http.post('/logout')
+          .then(response => {
+            this.$root.user = {}
+            this.$router.push('/login')
+          })
+          .catch(error => {
+            console.error(error)
+            eventBus.$emit('info.snackbar', error.response.data.error)
+          })
+      }
+    },
   }
 </script>
 
