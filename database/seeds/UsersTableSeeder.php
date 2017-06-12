21dc22be35b60e1799ecdd52e78245d763e1470e
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2017-06-12 10:40:16 +0200

    add users seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $coma = \App\Company::firstOrCreate([
+            'name' => 'coma AG',
+        ]);
+
         $admin = DB::table('users')->where('email', 'admin@patch-day.dev')
             ->first();
         if (!$admin) {
@@ -18,8 +22,31 @@ class UsersTableSeeder extends Seeder
                 'name' => 'Admin',
                 'email' => 'admin@patch-day.dev',
                 'password' => bcrypt('secret'),
-                'company_id' => \App\Company::first()->id,
+                'company_id' => $coma->id,
                 'role' => 'admin',
+            ]);
+        }
+
+        $client = DB::table('users')->where('email', 'client@example.com')
+            ->first();
+        if (!$client) {
+            DB::table('users')->insert([
+                'name' => 'Fake Fakerson',
+                'email' => 'client@example.com',
+                'password' => bcrypt('secret'),
+                'company_id' => \App\Company::first()->id,
+                'role' => 'client',
+            ]);
+        }
+
+        $companies = \App\Company::where(
+            'id', '!=', $coma->id
+        )->get();
+
+        foreach($companies as $company) {
+            factory(\App\User::class, rand(1, 5))->create([
+                'role' => 'client',
+                'company_id' => $company->id,
             ]);
         }
     }
