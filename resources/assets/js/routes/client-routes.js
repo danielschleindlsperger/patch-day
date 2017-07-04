44d489db7977a6a698462433fe41cd976bbc4f29
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2017-07-04 11:39:08 +0200

    different routes for admins and clients

---
+++ resources/assets/js/routes/client-routes.js
@@ -0,0 +1,33 @@
+// components
+import Dashboard from 'pages/Dashboard'
+import NotFoundPage from 'pages/404'
+import Company from 'pages/company/Company'
+import Project from 'pages/project/Project'
+import Protocol from 'pages/protocol/Protocol'
+// patch days
+import PatchDayIndex from 'pages/patch-day/Index'
+import PatchDayDetail from 'pages/patch-day/Detail'
+
+export default [
+  {
+    path: '/', component: Dashboard,
+  },
+  {
+    path: '/companies/:id', component: Company
+  },
+  {
+    path: '/projects/:id', component: Project,
+  },
+  {
+    path: '/protocols/:id', component: Protocol,
+  },
+  {
+    path: '/patch-days', component: PatchDayIndex,
+  },
+  {
+    path: '/patch-days/:id', component: PatchDayDetail,
+  },
+  {
+    path: '*', component: NotFoundPage, name: 'not-found'
+  },
+]
