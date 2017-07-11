1b9e7fac7c86b06d5b74a5d0ff72ebb0a7cb5977
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2017-07-11 15:33:54 +0200

    extract user-table component

--- resources/assets/js/pages/user/Index.vue
+++ resources/assets/js/pages/user/Index.vue
@@ -2,46 +2,7 @@
     <div>
         <v-container>
             <h1 class="display-2 text-xs-center">Users</h1>
-
-            <!-- TODO: fix search for company name -->
-            <div class="action-wrapper">
-                <v-text-field
-                        append-icon="search"
-                        label="Search"
-                        single-line
-                        hide-details
-                        v-model="search"
-                ></v-text-field>
-            </div>
-            <v-card>
-                <v-data-table
-                        :headers="tableHeaders"
-                        :items="users"
-                        :search="search"
-                        hide-actions
-                        class="elevation-1"
-                >
-                    <template slot="items" scope="props">
-                        <td>
-                            <router-link :to="'/users/' + props.item.id">
-                                {{ props.item.name }}
-                            </router-link>
-                        </td>
-                        <td>
-                            <router-link
-                                    :to="'/companies/' + props.item.company.id">
-                                {{ props.item.company.name }}
-                            </router-link>
-                        </td>
-                        <td>
-                            {{ props.item.role }}
-                        </td>
-                        <td class="text-xs-right">
-                            {{ props.item.created_at | DateTime }}
-                        </td>
-                    </template>
-                </v-data-table>
-            </v-card>
+            <user-table :users="users"></user-table>
         </v-container>
         <fab :fabActions="fabActions"></fab>
     </div>
@@ -49,12 +10,13 @@
 
 <script>
   import eventBus from 'components/event-bus'
-  import filters from 'mixins/filters'
   import Fab from 'pages/user/Fab'
+  import UserTable from 'pages/user/components/UserTable'
 
   export default {
     components: {
       Fab,
+      UserTable,
     },
     data() {
       return {
@@ -75,33 +37,8 @@
             },
           },
         ],
-        tableHeaders: [
-          {
-            text: 'Name',
-            left: true,
-            value: 'name',
-          },
-          {
-            text: 'Company',
-            value: 'company.name',
-            sortable: true,
-            left: true,
-          },
-          {
-            text: 'Role',
-            value: 'role',
-            sortable: true,
-            left: true,
-          },
-          {
-            text: 'Created',
-            value: 'id',
-            sortable: true,
-          },
-        ]
       }
     },
-    mixins: [filters],
     mounted() {
       this.getUsers()
     },
@@ -123,15 +60,3 @@
     }
   }
 </script>
-
-<style lang="scss" scoped>
-    .action-wrapper {
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-        > div {
-            width: 100%;
-            max-width: 200px;
-        }
-    }
-</style>
