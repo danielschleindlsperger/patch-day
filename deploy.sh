8a89b3396dce30f9df4b6f6be72abfb26632fc0a
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2017-07-31 17:58:39 +0200

    change rsync, add exit code to deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,6 @@ mv deploy-key ~/.ssh/id_rsa
 
 rsync --version
 
-rsync -az --force --delete-after --progress --exclude-from=rsync_exclude.txt -e
-"ssh -p22" $TRAVIS_BUILD_DIR deploy@45.63.116.5:/var/www/patch-day
+rsync -r --delete-after --quiet --exclude-from=rsync_exclude.txt $TRAVIS_BUILD_DIR deploy@45.63.116.5:/var/www/patch-day
+
+exit 0
