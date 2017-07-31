7f2f53155eac93c40bffea49154cacade41cc1b1
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2017-07-31 14:38:42 +0200

    fix rsync deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,25 +8,5 @@ rm deploy-key.enc
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
 
-$ERRORSTRING="Error. Please make sure you've indicated correct parameters"
-if [ $# -eq 0 ]
-    then
-        echo $ERRORSTRING;
-elif [ $1 == "live" ]
-    then
-        if [[ -z $2 ]]
-            then
-                echo "Running dry-run"
-                rsync --dry-run -az --force --delete --progress
-                --exclude-from=rsync_exclude.txt -e "ssh -p22" ./
-                deploy@45.63.116.5:/var/www/patch-day
-        elif [ $2 == "go" ]
-            then
-                echo "Running actual deploy"
-                rsync -az --force --delete --progress
-                --exclude-from=rsync_exclude.txt -e "ssh -p22" ./ deploy@45.63
-                .116.5:/var/www/website-name
-        else
-            echo $ERRORSTRING;
-        fi
-fi
+rsync -az --force --delete --progress --exclude-from=rsync_exclude.txt -e
+"ssh -p22" ./ deploy@45.63.116.5:/var/www/patch-day
