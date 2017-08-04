d08e082985921919e0b975b75dc07587adef2c83
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2017-08-04 11:31:33 +0200

    move back to deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-set -x
 
 # Don't need it anymore
-rm deploy-key.enc
 chmod 600 deploy-key
-mv deploy-key ~/.ssh/id_rsa
 
-rsync --version
+# dry run
+rsync -avzu --dry-run --exclude-from=".rsync-exclude.txt" --rsh="ssh -i /tmp/deploy_rsa" $TRAVIS_BUILD_DIR/ deploy@45.63.116.5:/var/www/patch-day
 
-rsync -r --delete-after --quiet --exclude-from=rsync_exclude.txt $TRAVIS_BUILD_DIR deploy@45.63.116.5:/var/www/patch-day
+# for real now
+rsync -avzu --exclude-from=".rsync-exclude.txt" --rsh="ssh -i /tmp/deploy_rsa" $TRAVIS_BUILD_DIR/ deploy@45.63.116.5:/var/www/patch-day
+
+# some optimization and migrations
+ssh -i /tmp/deploy_rsa deploy@45.63.116.5 'cd /var/www/patch-day &&  php artisan cache:clear && php artisan route:clear && php artisan route:cache && php artisan view:clear && php artisan optimize && php artisan migrate'
 
 exit 0
