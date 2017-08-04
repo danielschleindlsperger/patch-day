349f781fd5cf13a332f7be166a7df5dd44390937
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2017-08-04 13:11:04 +0200

    aasdfasdf

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-chmod 600 deploy-key
+openssl aes-256-cbc -K $encrypted_05c77b57f590_key -iv $encrypted_05c77b57f590_iv -in deploy-key.enc -out /tmp/deploy_rsa -d
+eval "$(ssh-agent -s)"
+rm deploy-key.enc
+chmod 600 /tmp/deploy_rsa
+ssh-add /tmp/deploy_rsa
 
 # dry run
 rsync -avzu --dry-run --exclude-from=".rsync-exclude.txt" --rsh="ssh -i /tmp/deploy_rsa" ./ deploy@45.63.116.5:/var/www/patch-day
