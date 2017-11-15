6c6804c9fa0b292fb5f80bd3524596713205185f
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2017-11-15 20:08:27 +0100

    fix rsync exclude path

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ chmod 600 /tmp/deploy_rsa
 ssh-add /tmp/deploy_rsa
 
 # dry run
-rsync -avzu --dry-run --exclude-from="/var/www/patch-day/.rsync-exclude.txt" --rsh="ssh -i /tmp/deploy_rsa" ./ deploy@45.63.116.5:/var/www/patch-day
+rsync -avzu --dry-run --exclude-from="./.rsync-exclude.txt" --rsh="ssh -i /tmp/deploy_rsa" ./ deploy@45.63.116.5:/var/www/patch-day
 
 # for real now
-rsync -avzu --exclude-from="/var/www/patch-day/.rsync-exclude.txt" --rsh="ssh -i /tmp/deploy_rsa" ./ deploy@45.63.116.5:/var/www/patch-day
+rsync -avzu --exclude-from="./.rsync-exclude.txt" --rsh="ssh -i /tmp/deploy_rsa" ./ deploy@45.63.116.5:/var/www/patch-day
 
 # some optimization and migrations
 ssh -i /tmp/deploy_rsa deploy@45.63.116.5 'cd /var/www/patch-day &&  php artisan cache:clear && php artisan route:clear && php artisan route:cache && php artisan view:clear && php artisan migrate'
